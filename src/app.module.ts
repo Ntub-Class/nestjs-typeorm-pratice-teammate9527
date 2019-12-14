37136f4729761f40295c4aa99f38e0cd65c7cfbd
Your Name <you@example.com>
2019-12-14 16:58:21 +0800

    done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as ormconﬁg from './ormconfig';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(ormconﬁg)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
